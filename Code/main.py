d3b476819dc1416eb91b94b8f90b1e561811c502
lefa22 <66741521+lefa22@users.noreply.github.com>
2022-04-03 18:25:59 +0200

    main.py wie besprochen ergänzt

--- Code/main.py
+++ Code/main.py
@@ -15,7 +15,7 @@ def closeBanner(event):
 
 topBanner = tk.Frame(root, background="#d35c00")
 
-topBannerLabel = tk.Label(topBanner, text="Dieses Programm befindet sich noch in Bearbeitung.", bg="#d35c00", font=("standard", 11), anchor="center")
+topBannerLabel = tk.Label(topBanner, text="Dieses Programm befindet sich noch in Bearbeitung.", bg="#d35c00", font=("standard", 11), anchor="w")
 topBannerLabel.pack(expand=1, fill="both", padx=10, pady=10, side="left")
 
 topBannerX = tk.Label(topBanner, text="X", bg="#d35c00", font=("Fredoka", 12))
@@ -32,10 +32,24 @@ platzhalterLabel.pack(expand=1)
 
 
 
+menu = tk.Menu(root, borderwidth=2)
+
+filemenu = tk.Menu(menu, tearoff=0)
+filemenu.add_cascade(label="Schließen", command=root.destroy)
+
+modulemenu = tk.Menu(menu, tearoff=0)
+modulemenu.add_cascade(label="Noch keine Module vorhanden...", state="disabled", activebackground="#f0f0f0")
+
+menu.add_cascade(label="Datei", menu=filemenu)
+menu.add_cascade(label="Module", menu=modulemenu)
+
+
+
 topBannerX.bind("<ButtonPress-1>", closeBanner)
 
 
 
+root.config(menu=menu)
 root.title("Hescio")
 root.geometry("500x350")
 root.minsize(width=420, height=100)
