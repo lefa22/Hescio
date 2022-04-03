b0fb8150fb708fe0a27fbf5f9c9838ef466068f3
lefa22 <66741521+lefa22@users.noreply.github.com>
2022-04-03 17:52:42 +0200

    Update main.py
    Banner "Dieses Programm befindet sich noch in Bearbeitung." wurde hinzugefügt.

--- Code/main.py
+++ Code/main.py
@@ -1,14 +1,42 @@
 """
 main programm
 """
+
 import tkinter as tk
-from tkinter import messagebox
+
+
 
 root = tk.Tk()
 
-platzhalterLabel = tk.Label(root, text="Platzhalter")
+
+
+def closeBanner(event):
+    topBanner.pack_forget()
+
+topBanner = tk.Frame(root, background="#d35c00")
+
+topBannerLabel = tk.Label(topBanner, text="Dieses Programm befindet sich noch in Bearbeitung.", bg="#d35c00", font=("standard", 11), anchor="center")
+topBannerLabel.pack(expand=1, fill="both", padx=10, pady=10, side="left")
+
+topBannerX = tk.Label(topBanner, text="X", bg="#d35c00", font=("Fredoka", 12))
+topBannerX.pack(expand=0, fill="both", padx=20, pady=10, side="right")
+
+topBanner.pack(side="top", expand=0, fill="both")
+
+
+mainContent = tk.Frame(root)
+mainContent.pack(side="bottom", expand=1)
+
+platzhalterLabel = tk.Label(mainContent, text="Platzhalter")
 platzhalterLabel.pack(expand=1)
 
+
+
+topBannerX.bind("<ButtonPress-1>", closeBanner)
+
+
+
 root.title("Hescio")
-root.geometry("210x100")
+root.geometry("500x350")
+root.minsize(width=420, height=100)
 root.mainloop()
