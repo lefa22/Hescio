388e3ea2a95f17ecbbe6951a725b2e4d76e0bd7e
lefa22 <66741521+lefa22@users.noreply.github.com>
2022-04-09 16:14:56 +0200

    Update info.py

--- Code/info.py
+++ Code/info.py
@@ -1,9 +1,13 @@
+import tkinter as tk
+
+icon = 0
+
 def show():
-    import tkinter as tk
+    infoFenster = tk.Tk()
 
-    root = tk.Tk()
+    infoFenster.title("Info")
+    infoFenster.geometry(f"400x200+{int(infoFenster.winfo_screenwidth()/2-400/2)}+{int(infoFenster.winfo_screenheight()/2-200/2)}")
+    infoFenster.resizable(width=0, height=0)
+    infoFenster.mainloop()
 
-    root.title("Info")
-    root.geometry(f"400x200+{int(root.winfo_screenwidth()/2-400/2)}+{int(root.winfo_screenheight()/2-200/2)}")
-    root.resizable(width=0, height=0)
-    root.mainloop()
+#show()
